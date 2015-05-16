c1c834fd7bb3774a9135649393bd1ce9d22d840d
Michael Koppen <michael.koppen@googlemail.com>
2015-05-17 00:54:18 +0200

    property externalization

--- src/main/java/com/welovecoding/config/PersistenceConfig.java
+++ src/main/java/com/welovecoding/config/PersistenceConfig.java
@@ -1,7 +1,16 @@
 package com.welovecoding.config;
 
+import com.welovecoding.data.author.Author;
+import com.welovecoding.data.category.Category;
+import com.welovecoding.data.playlist.Playlist;
+import com.welovecoding.data.user.entity.User;
+import com.welovecoding.data.video.Video;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -16,6 +25,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
+
 /**
  * Spring Data Demos:	https://github.com/gordonad/spring-data-demos Spring
  * Profiles:
@@ -24,46 +34,28 @@ import java.util.Properties;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {
-  "com.welovecoding.data.news",
-  "com.welovecoding.data.account",
   "com.welovecoding.data.author",
   "com.welovecoding.data.category",
   "com.welovecoding.data.playlist",
   "com.welovecoding.data.user",
   "com.welovecoding.data.video"
 })
+@PropertySources({
+  @PropertySource("classpath:database.properties")
+})
 public class PersistenceConfig {
+
+  @Autowired
+  private Environment env;
 
   @Bean
   public DataSource dataSource() {
     DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-    dataSource.setUrl("jdbc:mysql://localhost:3306/welovecoding");
-    dataSource.setUsername("root");
-    dataSource.setPassword("");
+    dataSource.setDriverClassName(env.getProperty("wlc.database.driver.classname"));
+    dataSource.setUrl(env.getProperty("wlc.database.url"));
+    dataSource.setUsername(env.getProperty("wlc.database.username"));
+    dataSource.setPassword(env.getProperty("wlc.database.password"));
     return dataSource;
-  }
-
-  @Bean
-  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-    em.setDataSource(dataSource());
-    // scan for entities
-    em.setPackagesToScan(
-      "com.welovecoding.data.news",
-      "com.welovecoding.data.account",
-      "com.welovecoding.data.author",
-      "com.welovecoding.data.category",
-      "com.welovecoding.data.playlist",
-      "com.welovecoding.data.user",
-      "com.welovecoding.data.video"
-    );
-
-    JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-    em.setJpaVendorAdapter(vendorAdapter);
-    em.setJpaProperties(additionalProperties());
-
-    return em;
   }
 
   @Bean
@@ -78,14 +70,34 @@ public class PersistenceConfig {
     return new PersistenceExceptionTranslationPostProcessor();
   }
 
-  Properties additionalProperties() {
-    return new Properties() {
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+    em.setDataSource(dataSource());
+    // scan for entities
+    em.setPackagesToScan(
+      Category.class.getPackage().getName(),
+      Playlist.class.getPackage().getName(),
+      Video.class.getPackage().getName(),
+      Author.class.getPackage().getName(),
+      User.class.getPackage().getName()
+    );
+
+    JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    em.setJpaVendorAdapter(vendorAdapter);
+
+    em.setJpaProperties(new Properties() {
       {
         setProperty("hibernate.hbm2ddl.auto", "update");
         setProperty("hibernate.vendor", "MYSQL");
         setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
         setProperty("hibernate.globally_quoted_identifiers", "true");
       }
-    };
+    });
+
+    return em;
   }
 }
+
+
+
