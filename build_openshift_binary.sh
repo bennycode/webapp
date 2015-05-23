959f27fc2acd5b27e3531348cbd2d836bf2853f6
Michael Koppen <michael.koppen@googlemail.com>
2015-05-23 17:23:42 +0200

    binary deployment

--- build_openshift_binary.sh
+++ build_openshift_binary.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#/usr/local/bin/mvn
-mvn install -DskipTests=true -Dmaven.javadoc.skip=true -Dwlc.database.driver.classname=<jdbc_driver> -Dwlc.database.url=<jdbc_address> -Dwlc.database.username=<db_username> -Dwlc.database.password=<db_password> -Popenshift -B -V
+ls -la
+
+/usr/local/bin/mvn clean install -DskipTests=true -Dmaven.javadoc.skip=true -Dwlc.database.driver.classname=driver -Dwlc.database.url=adress -Dwlc.database.username=username -Dwlc.database.password=password -Popenshift -B -V
 
 # Use war name 'ROOT.war' if you would like to deploy to the root path of your server
-cp webapps/ROOT.war binary/dependencies/jbossews/webapps/<newname>.war
-cp -r .openshift/* binary/repo/.openshift/
+cp webapps/ROOT.war binary/dependencies/jbossews/webapps/ROOT.war
+cp -r .openshift/* binary/repo/.openshift
 
 cd binary
 tar -czvf ../webapps/binary.tar.gz ./
