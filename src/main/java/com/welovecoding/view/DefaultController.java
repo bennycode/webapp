e3915c7ee24c85388d22476883573a0a784803cb
Michael Koppen <michael.koppen@googlemail.com>
2015-05-24 13:35:50 +0200

    configured Spring MVC JSP Dispatcher Servlet and Controller

--- src/main/java/com/welovecoding/view/DefaultController.java
+++ src/main/java/com/welovecoding/view/DefaultController.java
@@ -1,21 +1,36 @@
 package com.welovecoding.view;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
 
   @RequestMapping(value = "/", method = RequestMethod.GET)
-  public String index(ModelMap map) {
-    map.put("msg", "Hello Spring 4 Web MVC!");
-    return "index";
+  public ModelAndView index() {
+    ModelAndView model = new ModelAndView("index");
+    model.addObject("msg", "hello world");
+    model.setViewName("index");
+    return model;
   }
-//    @RequestMapping(value = "/", method = RequestMethod.GET)
-//    public String index() {
-//	return "redirect:/static/index.html";
-//    }
+
+  @RequestMapping(value = "/debug", method = RequestMethod.GET)
+  public ModelAndView debug() {
+    ModelAndView model = new ModelAndView("debug");
+    model.addObject("msg", "hello world");
+    model.setViewName("debug");
+    return model;
+  }
+
+  @RequestMapping(value = "/snoop", method = RequestMethod.GET)
+  public ModelAndView snoop() {
+    ModelAndView model = new ModelAndView("snoop");
+    model.addObject("msg", "hello world");
+    model.setViewName("snoop");
+    return model;
+  }
+
 
 }
