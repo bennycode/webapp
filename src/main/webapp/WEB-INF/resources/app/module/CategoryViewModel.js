b747aa3acf4155e1703d69048f2328882a3aa71d
Benny Neugebauer <bn@bennyn.de>
2015-05-24 20:30:56 +0200

    Changed "continuation indent size" to "2" (confirm to IntelliJ defaults).

--- src/main/webapp/WEB-INF/resources/app/module/CategoryViewModel.js
+++ src/main/webapp/WEB-INF/resources/app/module/CategoryViewModel.js
@@ -33,13 +33,13 @@ function CategoryViewModel($http) {
   var self = this;
 
   $http.get('api/v1/categories')
-          .then(function (result) {
-            var categories = arrayize(result.data);
-            categories.forEach(function (categoryPayload) {
-              self.categories.push(new Category(categoryPayload));
-            });
-          })
-          .catch(function (result) {
-            console.log('Error', result);
-          });
+    .then(function (result) {
+      var categories = arrayize(result.data);
+      categories.forEach(function (categoryPayload) {
+        self.categories.push(new Category(categoryPayload));
+      });
+    })
+    .catch(function (result) {
+      console.log('Error', result);
+    });
 }
