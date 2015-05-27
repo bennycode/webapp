997c2f64b11fb79cb14d87d8beae118e8b4c0f43
Michael Koppen <michael.koppen@googlemail.com>
2015-05-27 17:39:59 +0200

    disabled security temporarily

--- src/main/java/com/welovecoding/config/SecurityContext.java
+++ src/main/java/com/welovecoding/config/SecurityContext.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,7 +17,7 @@ import org.springframework.social.security.SocialUserDetailsService;
 import org.springframework.social.security.SpringSocialConfigurer;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityContext extends WebSecurityConfigurerAdapter {
 
   @Autowired
