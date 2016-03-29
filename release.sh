1db21f9c44df37ca21026beab9ffce2e83ce2d99
Michael Koppen <michael.koppen@googlemail.com>
2016-03-29 16:13:08 +0200

    Fixes bash multiline

--- release.sh
+++ release.sh
@@ -1,14 +1,12 @@
-#mvn -Pdev,production,standalone clean install /
-#    -DskipTests=true /
-#    -Dmaven.javadoc.skip=true /
-#    -Dwlc.database.driver.classname=com.mysql.jdbc.Driver /
-#    -Dwlc.database.url=$DB_URL /
-#    -Dwlc.database.username=$DB_USERNAME /
-#    -Dwlc.database.password=$DB_PASSWORD /
+#mvn -Pdev,production,standalone clean install \
+#    -DskipTests=true \
+#    -Dmaven.javadoc.skip=true \
+#    -Dwlc.database.driver.classname=com.mysql.jdbc.Driver \
+#    -Dwlc.database.url=$DB_URL \
+#    -Dwlc.database.username=$DB_USERNAME \
+#    -Dwlc.database.password=$DB_PASSWORD \
 #    -B -V
-mvn -Pdev,production,standalone clean install /
-	-DskipTests=true /
-    -Dmaven.javadoc.skip=true /
+mvn -Pdev,production,standalone clean install antrun:run@prepare-eb-bundle assembly:assembly@package-eb-bundle \
+	-DskipTests=true \
+  -Dmaven.javadoc.skip=true \
 	-B -V
-mvn -Pdev,production,standalone antrun:run@prepare-eb-bundle -DskipTests=true
-mvn -Pdev,production,standalone assembly:assembly@package-eb-bundle -DskipTests=true
