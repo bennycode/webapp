20d78edea2695b7f2024c8d28d6992dc2dcf3537
Michael Koppen <michael.koppen@googlemail.com>
2016-03-30 16:17:34 +0200

    Fixes init SQL for MySQL

--- src/main/resources/schema_hsqldb.sql
+++ src/main/resources/schema_hsqldb.sql
@@ -15,7 +15,7 @@ create table oauth_client_details (
 
 create table oauth_client_token (
   token_id VARCHAR(256),
-  token BLOB,
+  token LONGVARBINARY,
   authentication_id VARCHAR(256) PRIMARY KEY,
   user_name VARCHAR(256),
   client_id VARCHAR(256)
@@ -23,22 +23,22 @@ create table oauth_client_token (
 
 create table oauth_access_token (
   token_id VARCHAR(256),
-  token BLOB,
+  token LONGVARBINARY,
   authentication_id VARCHAR(256) PRIMARY KEY,
   user_name VARCHAR(256),
   client_id VARCHAR(256),
-  authentication BLOB,
+  authentication LONGVARBINARY,
   refresh_token VARCHAR(256)
 );
 
 create table oauth_refresh_token (
   token_id VARCHAR(256),
-  token BLOB,
-  authentication BLOB
+  token LONGVARBINARY,
+  authentication LONGVARBINARY
 );
 
 create table oauth_code (
-  code VARCHAR(256), authentication BLOB
+  code VARCHAR(256), authentication LONGVARBINARY
 );
 
 create table oauth_approvals (
