160a77ef67df280284a61087e7b03932734e9f48
Michael Koppen <michael.koppen@googlemail.com>
2016-03-30 16:04:39 +0200

    Changes mvn release command to prod+standalone profile

--- release.sh
+++ release.sh
@@ -1,12 +1,4 @@
-#mvn -Pdev,production,standalone clean install \
-#    -DskipTests=true \
-#    -Dmaven.javadoc.skip=true \
-#    -Dwlc.database.driver.classname=com.mysql.jdbc.Driver \
-#    -Dwlc.database.url=$DB_URL \
-#    -Dwlc.database.username=$DB_USERNAME \
-#    -Dwlc.database.password=$DB_PASSWORD \
-#    -B -V
-mvn -Pdev,production,standalone clean install \
+mvn -Pproduction,standalone clean install \
 	-DskipTests=true \
   -Dmaven.javadoc.skip=true \
 	-B -V
